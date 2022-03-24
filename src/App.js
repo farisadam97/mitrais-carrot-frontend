cfd0c2cd64b5870a4fc7c96f92ead6daafb25637
Faris Adam <farisadam97@gmail.com>
2022-03-25 06:46:00 +0700

    first setup dev branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to mitrais frontend
         </a>
       </header>
     </div>
