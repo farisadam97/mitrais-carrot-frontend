b18c63af1817ace1f1e56dd829eb4a7463f5576f
arifka-rizki <arifkarizki.fakhriza@mitrais.com>
2022-03-29 13:42:16 +0700

    fetch bazaar item API

--- src/pages/staff/index/index.page.jsx
+++ src/pages/staff/index/index.page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavbarComponent from "../../../components/navbar/navbar.component";
 import Container from "../../container";
 import { Outlet } from "react-router-dom";
