5ca829d44fc076ab3b718b16aff717c3e81abdaf
Eva Rahmadanti <eva.rahmadanti@mitrais.com>
2022-03-31 09:23:25 +0700

    add history share page and send carrot page

--- src/store/api.js
+++ src/store/api.js
@@ -3,5 +3,11 @@ import { createAction } from "@reduxjs/toolkit";
 const GetBazaarItem = createAction("GetBazaarItem");
 const GetSharedHistory = createAction("GetSharedHistory");
 const GetRecentBirthday = createAction("GetRecentBirthday");
+const GetUsersList = createAction("GetUsersList");
 
-export const apiRequest = [GetBazaarItem, GetSharedHistory, GetRecentBirthday];
+export const apiRequest = [
+  GetBazaarItem,
+  GetSharedHistory,
+  GetRecentBirthday,
+  GetUsersList,
+];
