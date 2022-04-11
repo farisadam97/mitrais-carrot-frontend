a965acb12968efc7655bb6d03a15b673241a32d8
arifka-rizki <arifkarizki.fakhriza@mitrais.com>
2022-04-11 10:57:32 +0700

    assign role init

--- src/components/rootAdmin/assignRole.jsx
+++ src/components/rootAdmin/assignRole.jsx
@@ -1,7 +1,34 @@
 import ContainerContent from "../container/container.component";
-import { Table } from "react-bootstrap";
+import { Table, Button, Modal } from "react-bootstrap";
+import { connect } from "react-redux";
+import { GetUsersList } from "../../store/apiActions";
+import { useEffect, useState } from "react";
 
 const AssignRole = (props) => {
+    const [selectedUser, setSelecteduser] = useState(null);
+    const [show, setShow] = useState(false);
+    const [selectedUserName, setSelectedUserName] = useState("");
+
+    useEffect(() => {
+        props.getAlluser();
+    },[])
+
+    const handleShow = (id, name) => {
+        setSelecteduser(id);
+        setSelectedUserName(name);
+        setShow(true);
+    }
+
+    const handleClose = () => {
+        setSelecteduser(null);
+        setSelectedUserName("");
+        setShow(false);
+    }
+
+    const handleChange = () => {
+        handleClose();
+    }
+
     return (
         <ContainerContent title="Assign Role As Admin">
             <Table bordered hover striped>
@@ -15,22 +42,44 @@ const AssignRole = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                    </tr>
+                    {props.usersList.length > 0 ? (
+                        props.usersList.map((user, index) => {
+                            return (
+                                <tr key={user.id}>
+                                    <td>{index +1}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.role}</td>
+                                    <td>
+                                        <Button variant="primary" onClick={() => handleShow(user.id, user.name)}>Edit</Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    ):(<tr>
+                        <td colSpan={5} className="text-center">Data Not Found</td>
+                    </tr>)}
                 </tbody>
             </Table>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Confirmation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        Change <b>{selectedUserName}</b> role?
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={handleChange}>
+                        Change
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </ContainerContent>
     );
 }
@@ -38,15 +87,26 @@ const AssignRole = (props) => {
 
 const mapStateToProps = state => {
     return {
-
+        usersList : state.user.lists,
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         getAlluser: () => {
-            return dispatch()
+            return dispatch(GetUsersList({
+                url: `/user`,
+                method: 'POST',
+                data: {
+                    role: "9",
+                    fields: "id, name, username, roleId",
+                    pageNumber: "0",
+                    pageSize: "10",
+                    sortBy: "name",
+                    sortDir: "asc",
+                }
+            }))
         }
     }
 }
-export default AssignRole;
+export default connect(mapStateToProps, mapDispatchToProps)(AssignRole);
