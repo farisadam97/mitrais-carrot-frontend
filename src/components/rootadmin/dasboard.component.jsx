5f753a2a20b5b7e99e320c05f74dd68c3d3b2239
Muhamad Rizky Fauzan <mrfauzannn.99@gmail.com>
2022-04-20 08:39:13 +0700

    total carrot issue resolved

--- src/components/rootadmin/dasboard.component.jsx
+++ src/components/rootadmin/dasboard.component.jsx
@@ -2,37 +2,38 @@ import React from 'react'
 import ChartComponent from '../chart/chart.component'
 import { PureComponent } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import * as date from "../../utils/date/date.util";
+import { useState } from 'react';
 
-
-const DashboardRootAdmin = ({ lists, isLoading }) => {
-  console.log(lists)
-
-  const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-  ];
-
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
+const DashboardRootAdmin = ({ listsBasket, isLoading }) => {
+    const [getCreatedDate, setCreatedDate] = useState(date.GetCurrentDate("-"));
+    
   return (
     <div className='row'>
         <div className='col-md-6'>
-            <h5 className='my-3 box-title'>Staff's Birthday</h5>
+            <h5 className='my-3 box-title'>Carrot Information</h5>
             <table className='table table-bordered height-59' style={{width:"100%"}}>
                 <tbody id='rootadmin-dashboard-staff-birthday'>
-                {lists.length > 0 ? (
+                    <tr style={{width:"100%"}}>Date<sup></sup>
+                            <td style={{width:"30%"}} id="birthday-total_staff" class="text-right">{getCreatedDate}</td>
+                    </tr>
+                    <tr style={{width:"100%"}}>Total Carrot<sup></sup>
+                            <td style={{width:"30%"}} id="birthday-total_staff" class="text-right">{listsBasket[0].earnedAmount}</td>
+                    </tr>
+                    <tr style={{width:"100%"}}>Left Carrot<sup></sup>
+                            <td style={{width:"30%"}} id="birthday-total_staff" class="text-right">{listsBasket[0].currentAmount}</td>
+                    </tr>
+                {/* {lists.length > 0 ? (
                     lists.map(item => (
-                        // <tr key={item.id}>
-                        //     <td scope="row">{item.birthdayDate}</td>
-                        //     <td>{item.name}</td>
-                        //     <td>{item.position}</td>
-                        //     <td>{item.office}</td>
-                        //     <td>{item.email}</td>
-                        // </tr>
-                        <tr style="width:70%">Total number of staff<sup>*</sup>
-                            <td style="width:30%" id="birthday-total_staff" class="text-right">{item.pageSize}</td>
+                        <tr key={item.id}>
+                            <td scope="row">{item.birthdayDate}</td>
+                            <td>{item.name}</td>
+                            <td>{item.position}</td>
+                            <td>{item.office}</td>
+                            <td>{item.email}</td>
+                        </tr>
+                        <tr style={{width:"70%"}}>Total number of staff<sup>*</sup>
+                            <td style={{width:"30%"}} id="birthday-total_staff" class="text-right">{item.createdAt[0]}</td>
                         </tr>
                         
                     ))
@@ -48,7 +49,7 @@ const DashboardRootAdmin = ({ lists, isLoading }) => {
                             Data tidak ditemukan
                             </td>
                         </tr>
-                    )};
+                    )}; */}
                 </tbody>
             </table>
         </div>
--- src/pages/rootAdmin/dashboard/dashboard.root.page.jsx
+++ src/pages/rootAdmin/dashboard/dashboard.root.page.jsx
@@ -6,23 +6,26 @@ import Container from "../../container";
 import RootTabs from "../../../components/tab/rootAdmin.tab.component";
 import DashboardRootAdmin from "../../../components/rootAdmin/dasboard.component";
 import { connect } from 'react-redux';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
 
 const RootAdminDashboardPage = (props) => {
+  const cookies = new Cookies();
+  const [getToken, setToken] = useState(cookies.get('access_token'));
+  const [getId, setId] = useState(cookies.get('id'));
     useEffect(() => {
-        props.loadAnnualCarrot()
-        props.loadStaff()
-        console.log(props)
+        props.loadHarvest(getToken)
+        props.loadBasket(getId, getToken)
       }, [])
     return(
         <div className="">
             <Container>
-                <ContainerContent title={"DASHBOARD"}>
+                <ContainerContent title={""}>
                 <div className="row align-items-center" >
                     {/* <div className="col-md-6 text-end">
                         <a href="#" className="btn btn-success py-2 float-right"> Create Barn </a>
                     </div> */}
-                    <DashboardRootAdmin lists={props.lists} isLoading={props.isLoading} />
+                    {props.listsBasket[0] && props.lists[0] && <DashboardRootAdmin lists={props.lists} listsBasket={props.listsBasket} isLoading={props.isLoading} isLoadingBasket={props.isLoadingBasket} />}
                 </div>
                 </ContainerContent>
             </Container>
@@ -32,27 +35,53 @@ const RootAdminDashboardPage = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-      // lists: state.annualCarrot.lists,
-      // isLoading: state.annualCarrot.isLoading,
-      // error: state.annualCarrot.error,
-      // pagination: state.annualCarrot.pagination,
-      lists: state.usersList.lists,
-      isLoading: state.usersList.isLoading,
-      error: state.usersList.error,
-      pagination: state.usersList.pagination,
+      lists: state.harvestList.lists,
+      isLoading: state.harvestList.isLoading,
+      error: state.harvestList.error,
+      pagination: state.harvestList.pagination,
+      listsBasket: state.annualBasket.lists,
+      isLoadingBasket : state.annualBasket.isLoadingBasket,
+      errorBasket: state.annualBasket.errorBasket,
+      paginationBasket : state.annualBasket.paginationBasket,
+      // lists: state.annualBasket.lists,
+      // isLoading: state.annualBasket.isLoading,
+      // error: state.annualBasket.error,
+      // pagination: state.annualBasket.pagination,
+      // lists: state.usersList.lists,
+      // isLoading: state.usersList.isLoading,
+      // error: state.usersList.error,
+      // pagination: state.usersList.pagination,
     };
   };
   
   const mapDispatchToProps = (dispatch) => {
     return {
-      loadAnnualCarrot: () => {
+      loadHarvest: (token) => {
         return dispatch({
-          type: "GetAnnualCarrot",
+          type: "GetHarvest",
+          payload: {
+            url: "/carrot/annual",
+            method: "GET",
+            // data: {
+            //   pageNumber: "0",
+            //   pageSize: "10",
+            //   sortBy: "id",
+            //   sortDir: "asc",
+            // },
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          },
+        });
+      },
+      onPageChange: (pageNumber) => {
+        return dispatch({
+          type: "GetHarvest",
           payload: {
             url: "/carrot/annual",
             method: "GET",
             data: {
-              pageNumber: "0",
+              pageNumber: pageNumber - 1,
               pageSize: "10",
               sortBy: "id",
               sortDir: "asc",
@@ -60,25 +89,60 @@ const mapStateToProps = (state) => {
           },
         });
       },
-      loadStaff: () => {
+      loadBasket: (id, token) => {
         return dispatch({
-          type: "GetUsersList",
+          type: "GetAnnualBasket",
           payload: {
-            url: "/user",
+            url: `/basket/user/${id}`,
             method: "POST",
             data: {
-              role : "5",
-              fields:
-                "id, name, position, grades, office, email, status, resignDate",
-              pageNumber: "0",
-              pageSize: "100000",
-              sortBy: "id",
-              sortDir: "asc",
+              fields: "id, earned_amount, shared_amount, donate_amount, spent_amount, current_amount",
             },
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
           },
         });
       },
     };
   };
+
+  // const mapDispatchToProps = (dispatch) => {
+  //   return {
+  //     loadAnnualCarrot: () => {
+  //       return dispatch({
+  //         type: "GetAnnualBasket",
+  //         payload: {
+  //           url: `/basket/user/${id}`,
+  //           method: "POST",
+  //           data: {
+  //             fields: "id, earned_amount, shared_amount, donate_amount, spent_amount, current_amount",
+  //           },
+  //           headers: {
+  //             Authorization: `Bearer ${token}`
+  //           }
+  //         },
+  //       });
+  //     },
+  //     loadStaff: () => {
+  //       return dispatch({
+  //         type: "GetUsersList",
+  //         payload: {
+  //           url: "/user",
+  //           method: "POST",
+  //           data: {
+  //             role : "5",
+  //             fields:
+  //               "id, name, position, grades, office, email, status, resignDate",
+  //             pageNumber: "0",
+  //             pageSize: "100000",
+  //             sortBy: "id",
+  //             sortDir: "asc",
+  //           },
+  //         },
+  //       });
+  //     },
+  //   };
+  // };
   
 export default connect(mapStateToProps, mapDispatchToProps)(RootAdminDashboardPage);
